770b0f26eb80dcbdd5004729d8bde386baa0d54b
congialap5jack <kietcuchi148@gmail.com>
2025-02-24 17:00:15 +0700

    Add project files.

---
+++ bai15-TínhSoThoc/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace bai15_TínhSoThoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtô = new System.Windows.Forms.TextBox();
+            this.txtSoThoc = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btntinh = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtô
+            // 
+            this.txtô.Location = new System.Drawing.Point(125, 35);
+            this.txtô.Name = "txtô";
+            this.txtô.Size = new System.Drawing.Size(97, 20);
+            this.txtô.TabIndex = 0;
+            // 
+            // txtSoThoc
+            // 
+            this.txtSoThoc.Location = new System.Drawing.Point(125, 96);
+            this.txtSoThoc.Name = "txtSoThoc";
+            this.txtSoThoc.Size = new System.Drawing.Size(97, 20);
+            this.txtSoThoc.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập ô";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btntinh
+            // 
+            this.btntinh.Location = new System.Drawing.Point(125, 165);
+            this.btntinh.Name = "btntinh";
+            this.btntinh.Size = new System.Drawing.Size(89, 32);
+            this.btntinh.TabIndex = 2;
+            this.btntinh.Text = "tính";
+            this.btntinh.UseVisualStyleBackColor = true;
+            this.btntinh.Click += new System.EventHandler(this.btntinh_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = " tổng số thóc";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(338, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(208, 173);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btntinh);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSoThoc);
+            this.Controls.Add(this.txtô);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtô;
+        private System.Windows.Forms.TextBox txtSoThoc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btntinh;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ bai15-TínhSoThoc/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bai15_TínhSoThoc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btntinh_Click(object sender, EventArgs e)
+        {
+            int soO = Convert.ToInt32(txtô.Text);
+
+            double sothoc = 0;
+
+            if (soO > 64)
+            {
+                MessageBox.Show("Bàn cờ chỉ có 64 ô vui lòng nhập số < 64", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                return;
+            }
+            for (int i = 0; i < soO; i++)
+            {
+                sothoc += Math.Pow(2, i);
+                listBox1.Items.Add($" Ô thứ " + (i + 1) + " có " + Math.Pow(2, i) + "thóc");
+            }
+            txtSoThoc.Text = sothoc.ToString();
+
+        }
+
+        private void lstCacO_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
